49041648ffd203afedf9bb288e0f7b91c21eb183
Tomasz Szymański <151573418+Tejtone@users.noreply.github.com>
2025-04-10 16:07:06 +0200

    Create Task3.sql

---
+++ Second_Summer_Semester/Database_Systems/02 - T-SQL (procedury)/Task3.sql
@@ -0,0 +1,24 @@
+CREATE PROCEDURE task3
+    @dname VARCHAR(50),
+    @loc VARCHAR(20),
+    @dnumber INT OUTPUT
+AS
+BEGIN
+
+    DECLARE @newDeptno INT;
+
+    IF EXISTS (SELECT 1 FROM DEPT WHERE dname = @dname OR loc = @loc)
+BEGIN
+    THROW 51000, 'Podana nazwa lub lokalizacja istnieje', 1;
+END;
+
+    SELECT @newDeptno = MAX(deptno) + 10 FROM DEPT;
+    INSERT INTO DEPT (deptno, dname, loc)
+    VALUES (@newDeptno, @dname, @loc);
+    SET @dnumber = @newDeptno;
+
+END;
+
+DECLARE @dout INT;
+EXEC task3 @dname = 'HR', @loc = 'Kraków', @dnumber = @dout OUTPUT;
+PRINT 'Nowy numer działu: ' + CAST(@dout AS VARCHAR);
