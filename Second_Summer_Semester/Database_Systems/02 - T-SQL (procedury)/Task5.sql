d57f44d204f0d2b9930739900ed03b49d8881f08
Tomasz Szymański <151573418+Tejtone@users.noreply.github.com>
2025-04-10 16:47:54 +0200

    Create Task5.sql

---
+++ Second_Summer_Semester/Database_Systems/02 - T-SQL (procedury)/Task5.sql
@@ -0,0 +1,43 @@
+CREATE PROCEDURE task5
+    @minSalary INT,
+    @maxSalary INT
+AS
+BEGIN
+DECLARE all_ename CURSOR FOR
+SELECT empno, ename, sal FROM EMP;
+
+DECLARE @empno INT, @ename VARCHAR(20), @sal INT;
+
+OPEN all_ename;
+
+FETCH NEXT FROM all_ename INTO @empno, @ename, @sal;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    IF @sal < @minSalary
+    BEGIN
+        UPDATE EMP
+        SET sal = sal * 1.1
+        WHERE empno = @empno;
+
+        PRINT @ename + ' miał pensję poniżej ' + CONVERT(VARCHAR, @minSalary) + ' – nowa pensja: ' +
+              CONVERT(VARCHAR, @sal * 1.1);
+    END;
+    ELSE IF @sal > @maxSalary
+    BEGIN
+        UPDATE EMP
+        SET sal = sal * 0.9
+        WHERE empno = @empno;
+
+        PRINT @ename + ' miał pensję powyżej ' + CONVERT(VARCHAR, @maxSalary) +  '– nowa pensja: ' +
+              CONVERT(VARCHAR, @sal * 0.9);
+    END;
+
+    FETCH NEXT FROM all_ename INTO @empno, @ename, @sal;
+END;
+
+CLOSE all_ename;
+DEALLOCATE all_ename;
+END;
+
+EXEC task5 @minSalary = 1000, @maxSalary = 1500;
