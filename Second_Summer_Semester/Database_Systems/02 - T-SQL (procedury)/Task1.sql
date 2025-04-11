6f9fa985f16d0d726e0d2afefc33921896e89144
Tomasz Szymański <151573418+Tejtone@users.noreply.github.com>
2025-04-11 13:29:47 +0200

    Update Task1.sql

--- Second_Summer_Semester/Database_Systems/02 - T-SQL (procedury)/Task1.sql
+++ Second_Summer_Semester/Database_Systems/02 - T-SQL (procedury)/Task1.sql
@@ -1,3 +1,8 @@
+-- Utwórz procedurę, która znajdzie liczbę pracowników dla każdego działu. Jeśli liczba ta będzie niższa 
+-- od podanej, wyświetl następujący komunikat: "Dział numer X ma Y pracowników". Na końcu procedury 
+-- wyświetl informację o liczbie wyświetlonych komunikatów, a jeśli liczba jest niższa niż liczba 
+-- wszystkich działów, daj podwyżkę osobie o nazwie "KING". 
+
 CREATE PROCEDURE Task1
     @number INT
 AS
