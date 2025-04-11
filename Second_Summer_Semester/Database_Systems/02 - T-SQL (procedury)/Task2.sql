58d080e13ae378096d20e12ff2030f9b0635816e
Tomasz Szymański <151573418+Tejtone@users.noreply.github.com>
2025-04-11 13:30:14 +0200

    Update Task2.sql

--- Second_Summer_Semester/Database_Systems/02 - T-SQL (procedury)/Task2.sql
+++ Second_Summer_Semester/Database_Systems/02 - T-SQL (procedury)/Task2.sql
@@ -1,3 +1,6 @@
+-- Napisz prosty program w języku Transact-SQL. Zadeklaruj zmienną, przypisz do niej liczbę rekordów 
+-- w tabeli Emp i wydrukuj wynik za pomocą instrukcji PRINT - na przykład "Jest 10 pracowników”. 
+
     DECLARE @liczba_pracowników INT;
     SELECT @liczba_pracowników = COUNT(*)
     FROM emp;
